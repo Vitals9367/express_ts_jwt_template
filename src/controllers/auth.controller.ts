ef7d3e0eeeae1607065932374478ed63cf89656c
vitals9367 <vitalijus.alsauskas@gmail.com>
2021-12-01 21:15:03 +0200

    auth service added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,11 @@
+import express from "express";
+import jwt from 'jsonwebtoken';
+
 import db from "../models";
 import config from "../config/auth.config";
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
 
 import fieldsMissing from '../utils/fieldHelpers';
-import express from "express";
+import { comparePassword, hashPassword } from "../services/authService";
 
 const User = db.User;
 
@@ -16,19 +17,14 @@ export const signup = async (req:express.Request, res:express.Response) => {
 
   const missingField = fieldsMissing(fields,req.body);
 
-  if(req.userExists){
-    return res.status(403).send({ message: `Email already exists!` });
-  }
-
-  if(missingField){
-    return res.status(403).send({ message: `${missingField} field is missing!` });
-  }
+  if(req.userExists) return res.status(403).send({ message: `Email already exists!` });
+  if(missingField) return res.status(403).send({ message: `${missingField} field is missing!` });
 
   try {
     await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8)
+        password: hashPassword(req.body.password),
       })
       return res.status(202).send({ message: "User was registered successfully!" });
       
@@ -43,19 +39,13 @@ export const signin = async (req:express.Request, res:express.Response) => {
 
   const missingField = fieldsMissing(fields,req.body);
 
-  if(missingField){
-    return res.status(403).send({ message: `${missingField} field is missing!` });
-  }
-
-  if(!req.userExists){
-    return res.status(404).send({ message: `User does not exist!` });
-  }
+  if(missingField) return res.status(403).send({ message: `${missingField} field is missing!` });
+  if(!req.userExists) return res.status(404).send({ message: `User does not exist!` });
 
   try{
     const user = await db.User.findOne({where:{ email:req.body.email }})
-    if(!bcrypt.compareSync(req.body.password,user.password)){
-      return res.status(401).send({ message: `User credentials wrong!` });
-    }
+    if(!comparePassword(req.body.password, user.password)) return res.status(401).send({ message: `User credentials wrong!` });
+    
   }catch(err){
     return res.status(500).send({ message: "Server Error: " + err})
   }
---
+++ src/services/authService.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+
+export const hashPassword = (password:string):string => {
+
+    const saltRounds = 10;
+    const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    return hashedPassword;
+}
+
+export const comparePassword = (password:string, hashedPassword:string):boolean => {
+
+    return bcrypt.compareSync(password,hashedPassword)
+}
